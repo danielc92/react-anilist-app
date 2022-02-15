b3eb9000d887795839b959918f6b2bda3f3aa69e
daniel corcoran <danielc.92@outlook.com>
2022-02-15 17:38:12 +1100

    remove apollo

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import HomePage from "./pages/HomePage/HomePage";
 import reportWebVitals from "./reportWebVitals";
-import { InMemoryCache, ApolloClient, ApolloProvider } from "@apollo/client";
-import { BASE_URL } from "./settings/api";
+
 import { HashRouter, Route, Switch } from "react-router-dom";
-const client = new ApolloClient({
-  uri: BASE_URL,
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <HashRouter>
-        <Switch>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-        </Switch>
-      </HashRouter>
-    </ApolloProvider>
+    <HashRouter>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+      </Switch>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
